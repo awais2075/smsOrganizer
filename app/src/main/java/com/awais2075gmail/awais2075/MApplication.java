e236dea280d4f908d15207376e6057d7b0b4d97c
awais2075 <awais2075@gmail.com>
2018-01-18 12:02:24 +0500

    18-jan-18, 12:02 pm

---
+++ app/src/main/java/com/awais2075gmail/awais2075/MApplication.java
@@ -0,0 +1,10 @@
+package com.awais2075gmail.awais2075;
+
+import android.app.Application;
+
+/**
+ * Created by Muhammad Awais Rashi on 18-Jan-18.
+ */
+
+public class MApplication extends Application {
+}
